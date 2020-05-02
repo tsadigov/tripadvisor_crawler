453e75e10b66320a52d404e3f9436ff789dc894e
tsadigov <turalsadiqov497@gmail.com>
2020-05-02 17:30:58 +0400

    Ahmadov's change added to test

--- tripadvisor/spiders/tripadvisor_spider.py
+++ tripadvisor/spiders/tripadvisor_spider.py
@@ -1,7 +1,5 @@
 import scrapy
-import re, math
-from scrapy.loader import ItemLoader
-from tripadvisor.items import RestaurantItem, RestaurantInfoItem, RestaurantReviewItem
+from ..items import RestaurantItem, RestaurantInfoItem, RestaurantReviewItem
 import sys, os
 
 class TripadvisorSpider(scrapy.Spider):
@@ -9,54 +7,79 @@ class TripadvisorSpider(scrapy.Spider):
 
     restaurant_id_list = []
     page_offset = 0
+    offset_step = 30
     list_prev_len = 0
     list_cur_len = 0
     count = 0
+    next_url = ""
 
-    url = "https://www.tripadvisor.com/Restaurants-g293933-oa"+str(page_offset)+"-Azerbaijan.html#EATERY_LIST_CONTENTS"
+    url = "https://www.tripadvisor.com/Restaurants-g293933-Azerbaijan.html"
 
     def start_requests(self):
+        # self.driver = webdriver.Chrome(executable_path = chromeDriverPath)
         yield scrapy.Request(self.url, callback=self.parse)
 
+
     def parse(self, response):
+        """
+        From given url the main page response is scraped saving info about restaurants in the SQLite via pipelines.
+        The offset is increased each request time to access next 30 restaurants' data. While scraping a restaurant's
+        short info from main page, its link used to request restaurants' page which scraped in the parse_restaurants().
+        """
+        offset = response.request.url.split("-")[2]
+        oa = 0
+        if "oa" in offset:
+            oa = int(offset[2:])
 
-        items = RestaurantItem()
-        divs = response.css("div._1llCuDZj")
-        print("Page Count ----- >>> ", self.count)
+        if oa < self.page_offset:
+            pass
+        else:
+            items = RestaurantItem()
+            divs = response.css("div._1llCuDZj")
+            print("+"*100, self.count)
 
-        for div in divs:
-            link = div.css('a._15_ydu6b').attrib['href']
+            for div in divs:
+                link = div.css('a._15_ydu6b').attrib['href']
 
-            if link not in self.restaurant_id_list:
-                items['id'] = ''
-                items['name'] = link.split('-')[-2]
-                items['restaurant_type'] = div.css("div._2rmp5aUK div._3d9EnJpt span._1p0FLy4t::text").get()
-                try:
-                    items['restaurant_price'] = div.css("div._2rmp5aUK div._3d9EnJpt span._1p0FLy4t::text").getall()[1]
-                except:
-                    items['restaurant_price'] = ''
-                items['restaurantid_fk'] = "https://www.tripadvisor.com/"+str(link)
-                items['page'] = self.count
-                self.restaurant_id_list.append(link)
+                if link not in self.restaurant_id_list:
+                    items['id'] = ''
+                    items['name'] = link.split('-')[-2]
+                    items['restaurant_type'] = div.css("div._2rmp5aUK div._3d9EnJpt span._1p0FLy4t::text").get()
+                    try:
+                        items['restaurant_price'] = div.css("div._2rmp5aUK div._3d9EnJpt span._1p0FLy4t::text").getall()[1]
+                    except:
+                        items['restaurant_price'] = ''
+                    items['restaurantid_fk'] = "https://www.tripadvisor.com/"+str(link)
+                    items['page'] = self.count
+                    self.restaurant_id_list.append(link)
 
-                yield items
-                yield scrapy.Request("https://www.tripadvisor.com/"+str(link), self.parse_restaurants)
+                    yield items
+                    yield scrapy.Request("https://www.tripadvisor.com/"+str(link), self.parse_restaurants)
 
-        self.page_offset+=30
-        self.count += 1
+            self.page_offset += self.offset_step
+            self.count += 1
 
-        if self.page_offset < 61:
-            next_page = "https://www.tripadvisor.com/Restaurants-g293933-oa"+str(self.page_offset)+"-Azerbaijan.html#EATERY_LIST_CONTENTS"
-            yield scrapy.Request(next_page, self.parse)
+            if oa == 0:
+                url_parts = response.request.url.split("-Az")
+                self.next_page = url_parts[0] + "-oa" + str(self.page_offset) + "-Azerbaijan.html"
+            else:
+                url_parts = response.request.url.split("-oa")
+                self.next_page = url_parts[0] + "-oa" + str(self.page_offset) + "-Azerbaijan.html"
+            yield scrapy.Request(self.next_page, self.parse)
 
     def parse_restaurants(self, response):
         items = RestaurantInfoItem()
         items['id'] = response.request.url.split('-')[-2]
         items['link'] = response.request.url
         items['coordinate'] = ''#response.css("#neighborhood img::attr(src)").get()
-        items['image_url'] = response.css('div.prw_rup.prw_common_basic_image.photo_widget.large.landscape img').attrib['data-lazyurl']
-        items['address'] = response.css('span.restaurants-detail-overview-cards-LocationOverviewCard__detailLinkText--co3ei::text').get()
-        items['address'] = ''
+        try:
+            items['image_url'] = response.css('div.prw_rup.prw_common_basic_image.photo_widget.large.landscape img').attrib['data-lazyurl']
+        except:
+            items['image_url'] = ''
+        try:
+            items['address'] = response.css('span.restaurants-detail-overview-cards-LocationOverviewCard__detailLinkText--co3ei::text').get()
+        except:
+            items['address'] = ''
         try:
             items['phone_number'] =  response.css('a.restaurants-detail-top-info-TopInfo__infoCellLink--2ZRPG::text').getall()[-1]
         except:
@@ -92,12 +115,12 @@ class TripadvisorSpider(scrapy.Spider):
         items['price_range'] = ''
         items['cuisines'] = ''
         items['meals'] = ''
-        items['special_diets'] = 'something'
+        items['special_diets'] = ''
         try:
             details_title = response.css('div.restaurants-detail-overview-cards-DetailsSectionOverviewCard__categoryTitle--2RJP_::text').getall()
             details_text = response.css('div.restaurants-detail-overview-cards-DetailsSectionOverviewCard__tagText--1OH6h::text').getall()
-            for title,text in zip(details_title,details_text):
-                items[title.replace(' ','_').lower()] = text
+            for title, text in zip(details_title, details_text):
+                items[title.replace(' ', '_').lower()] = text
         except:
             pass
         yield items
